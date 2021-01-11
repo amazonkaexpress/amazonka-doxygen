da9b4742562be660002d665ea124c1ebf83e4b2d
amazonkaexpress <amazonkaexpress@users.noreply.github.com>
2021-01-11 22:34:12 +0000

    deploy: ce654c91db46942a98aace707ce77d9725e38e1f

---
+++ html/search/all_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['calibrate_2',['calibrate',['../classeye__tracking_1_1gaze__tracking_1_1gaze__tracking_1_1GazeTracking.html#a12c9f0ca71c0020ba4afd0a8c5f56d27',1,'eye_tracking::gaze_tracking::gaze_tracking::GazeTracking']]],
+  ['calibration_3',['Calibration',['../classeye__tracking_1_1gaze__tracking_1_1calibration_1_1Calibration.html',1,'eye_tracking::gaze_tracking::calibration']]],
+  ['contributing_20to_20amazonka_20express_20software_4',['Contributing to Amazonka Express software',['../md_CONTRIBUTING.html',1,'']]]
+];
---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['amazonka_2dexpress_2dsoft_80',['amazonka-express-soft',['../index.html',1,'']]]
+];
