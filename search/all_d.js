058a77cb5d4b938c51e826f582329e770d45ac3b
amazonkaexpress <amazonkaexpress@users.noreply.github.com>
2021-01-11 21:26:12 +0000

    deploy: a1a6c4d661e91d28e167aff3f87183770b142fbd

---
+++ search/all_d.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['asgi_35',['asgi',['../namespaceteacher__app__config_1_1asgi.html',1,'teacher_app_config']]],
+  ['settings_36',['settings',['../namespaceteacher__app__config_1_1settings.html',1,'teacher_app_config']]],
+  ['teacherappconfig_37',['TeacherAppConfig',['../classteacher__app_1_1apps_1_1TeacherAppConfig.html',1,'teacher_app::apps']]],
+  ['threshold_38',['threshold',['../classeye__tracking_1_1gaze__tracking_1_1calibration_1_1Calibration.html#a5a24c3e1fd3c29f12393220507d4b528',1,'eye_tracking::gaze_tracking::calibration::Calibration']]],
+  ['urls_39',['urls',['../namespaceteacher__app__config_1_1urls.html',1,'teacher_app_config']]],
+  ['wsgi_40',['wsgi',['../namespaceteacher__app__config_1_1wsgi.html',1,'teacher_app_config']]]
+];
---
+++ search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['contributing_20to_20amazonka_20express_20software_81',['Contributing to Amazonka Express software',['../md_CONTRIBUTING.html',1,'']]]
+];
